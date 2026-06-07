bf21b7d069efd6856499549b664b171e13a9d2d2
punitayare <ayarepunit7@gmail.com>
2026-06-08 01:31:32 +0530

    added access control and login feature

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,4 +4,15 @@ const api = axios.create({
   baseURL: "https://support-crm-q58l.onrender.com",
 });
 
+// 🔥 attach token automatically
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
