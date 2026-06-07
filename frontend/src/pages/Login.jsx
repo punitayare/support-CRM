488f4dd575a4a348bb71864c854c39e132b7c3ae
punitayare <ayarepunit7@gmail.com>
2026-06-08 00:28:48 +0530

    added access control and login feature

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -11,7 +11,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/auth/login", {
+      const res = await fetch("http://support-crm-q58l.onrender.com/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
