7f0986658e2cda7f7fd806e90f890eba493d36c8
punitayare <ayarepunit7@gmail.com>
2026-06-08 14:46:29 +0530

    aded localhost

--- backend/app/routers/tickets.py
+++ backend/app/routers/tickets.py
@@ -84,7 +84,7 @@ def get_all_tickets_route(
             "subject": t.subject,
             "status": t.status,
             "created_at": t.created_at,
-
+ "customer_email": t.customer_email,
             # UI DISPLAY FIELDS
             "customer_name": t.customer_name,
             "assigned_to": t.assigned_to,
